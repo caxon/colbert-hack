c806c7d1db2f0f05c706728d05b6a899fb2ee6d5
caxon <caxon26@gmail.com>
2021-03-14 00:49:34 -0500

    second commit?

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/createRepository")
+async def read_item(user: str, repo_name: str):
+    return {"status": f"Repo was created for user {user} with name {repo_name}"}
